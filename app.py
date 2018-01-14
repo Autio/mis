b48b67e208694e52d0a7a574841a8ecbee4ab180
Autio <petri.mikael.autio@gmail.com>
2018-01-14 18:15:55 +0000

    Add MySQL db and registration forms

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 Articles = Articles()
@@ -19,6 +22,24 @@ def articles():
 @app.route ('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
+
+class RegisterForm(Form):
+    name = StringField(u'Name', validators=[validators.Length(min=1, max=50)])
+    username = StringField(u'Name', validators = [validators.Length(min=4, max= 25)])
+    email = StringField(u'Email', validators = [validators.Length(min=6, max= 50)])
+    password = PasswordField(u'Password', validators = [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        x =1
+        # What the user does in the form
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug = True)

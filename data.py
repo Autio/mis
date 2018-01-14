77f4a3e78c1864162e03f76806eb7169cb35286b
Autio <petri.mikael.autio@gmail.com>
2018-01-14 17:07:25 +0000

    Added article listing

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+    {
+        'id': 1,
+        'title': 'Article One',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'Petri',
+        'create_date': '2018-01-14'
+    },
+    {
+        'id': 2,
+        'title': 'Article Two',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'Wendy',
+        'create_date': '2018-01-14'
+    },
+    {
+        'id': 3,
+        'title': 'Article Three',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'author': 'Mau',
+        'create_date': '2018-01-14'
+    }
+    ]
+    return articles

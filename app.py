3cc081472b1e42e12737dcc1c7286811b6136aaf
Autio <petri.mikael.autio@gmail.com>
2018-01-17 14:13:09 +0000

    Registration form saves in database. Project form created

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, DateField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD']='1555'
+app.config['MYSQL_DB']='misflaskapp'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+#init MYSQL
+mysql = MySQL(app)
+
 Articles = Articles()
 
 @app.route('/')
@@ -33,13 +43,66 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
+class ProjectForm(Form):
+    projectName = StringField(u'ProjectName', validators=[validators.Length(min=5, max=140)])
+    projectStartDate = DateField(u'Start date YYYY-MM-DD', validators=[validators.InputRequired()])
+    projectEndDate = DateField(u'End date YYYY-MM-DD', validators=[validators.InputRequired()])
+    projectCode = StringField(u'Project code', validators=[validators.Length(min=3, max=5)])
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html', form=form)
+
         # What the user does in the form
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        redirect(url_for('index'))
+
     return render_template('register.html', form=form)
 
+@app.route('/project', methods=['GET', 'POST'])
+def project():
+    form = ProjectForm(request.form)
+    if request.method == 'POST' and form.validate():
+
+        # What the user does in the form
+        projectName = form.projectName.data
+        projectStartDate = form.projectStartDate.data
+        projectEndDate = form.projectEndDate.data
+        projectCode = form.projectCode.data
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO projects(projectName, projectStartDate, projectEndDate, projectCode) VALUES(%s, %s, %s, %s)", (projectName, projectStartDate, projectEndDate, projectCode))
+
+        # Commit to DB
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Project information has been stored', 'success')
+
+        redirect(url_for('index'))
+
+    return render_template('project.html', form=form)
+
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug = True)
